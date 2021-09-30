fe6744e58807ca3fb29de9392afdeb8304a7c2ab
João Henrique <joao@mentat.solutions>
2021-09-30 15:00:51 -0300

    Complete challange

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    # length(list)
+    sum_list(list, 0)
+  end
+
+  defp sum_list([], acc) do
+    acc
+  end
+
+  defp sum_list([_head | tail], acc) do
+    sum_list(tail, acc + 1)
   end
 end
